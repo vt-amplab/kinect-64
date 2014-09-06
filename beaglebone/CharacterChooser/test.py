1016ab0ffde5a8d94ec5a2897c3d420c4e82a005
Jacob Yacovelli <jjyacovelli@gmail.com>
2014-09-06 16:36:47 -0400

    addded more tokens to test

--- beaglebone/CharacterChooser/test.py
+++ beaglebone/CharacterChooser/test.py
@@ -21,7 +21,7 @@ for cnt in contours1:
     approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
     area = cv2.contourArea(approx)
 
-    if (area < 15000) and (area > 1000) and (len(approx) < 25):
+    if (area < 5000) and (area > 2000) and (len(approx) < 25):
         print area
         print len(approx)
         cv2.drawContours(img, cnt, -1, (0, 255, 0), 5)
@@ -29,11 +29,16 @@ for cnt in contours1:
         if (len(approx) >= 13) and (len(approx) <= 15) and (area > 3350) and (area < 3600):
             print "DK"
             cv2.drawContours(img, [cnt], 0, (255, 0, 0), -1)
-        elif (len(approx) >= 13) and (len(approx) <= 15) and (area > 3350) and (area < 3800):
+        elif (len(approx) >= 11) and (len(approx) <= 16) and (area > 3350) and (area < 3800):
             print "Yoshi"
             cv2.drawContours(img, [cnt], 0, (0, 0, 255), -1)
-        elif (len(approx) >= 13) and (len(approx) <= 15) and (area > 3350) and (area < 3800):
-            p=1
+        elif (len(approx) >= 20) and (len(approx) <= 24) and (area > 3600) and (area < 4000):
+            print "Falcon"
+            cv2.drawContours(img, [cnt], 0, (0, 100, 255), -1)
+        elif (len(approx) >= 10) and (len(approx) <= 12) and (area > 2200) and (area < 2500):
+            print "Pika"
+            cv2.drawContours(img, [cnt], 0, (100, 100, 255), -1)
+
 
 cv2.imshow('contour', img)
 cap.release()
